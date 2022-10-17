655e802467d731afc2435c6f9716f003804a30a0
Juan Diego <jugarzonbe@unal.edu.co>
2022-10-17 17:51:44 -0500

    .

--- accounts.py
+++ accounts.py
@@ -5,11 +5,15 @@ class Accounts():
     with open("accounts.txt") as file:
       self.listAccounts = [Accounts.replace("\n","").split(",") for Accounts in file.readlines()]
       self.listAccounts.pop(0)
-      self.dictAccounts = {Account[1]:[Account[0],Account[2]] for Account in self.listAccounts }
+      self.dictAccounts = {Account[1]:(Account[0],Account[2]) for Account in self.listAccounts }
 
   def showAccountsByID(self,id):
-    account = {self.dictAccounts.get(account) for account in self.dictAccounts}
-    print(account,id)
-    time.sleep(50)
+    accounts = {}
+    for i in self.dictAccounts:
+      x = self.dictAccounts.get(i)
+      if x[0] == id:
+        accounts.update({i:x})
+    print( accounts)
+    time.sleep(10)
             
         
