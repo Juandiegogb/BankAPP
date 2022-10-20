850d6926663f00ed404c45efc84a590b8a337196
Juan Diego <jugarzonbe@unal.edu.co>
2022-10-19 22:31:38 -0500

    Extension

--- accounts.py
+++ accounts.py
@@ -1,19 +1,54 @@
 import time
+import os
 
 class Accounts():
   def __init__(self):
-    with open("accounts.txt") as file:
+    with open("accounts.txt","r") as file:
       self.listAccounts = [Accounts.replace("\n","").split(",") for Accounts in file.readlines()]
       self.listAccounts.pop(0)
-      self.dictAccounts = {Account[1]:(Account[0],Account[2]) for Account in self.listAccounts }
+      self.dictAccounts = {Account[0]:[Account[1],Account[2]] for Account in self.listAccounts }
 
-  def showAccountsByID(self,id):
-    accounts = {}
-    for i in self.dictAccounts:
-      x = self.dictAccounts.get(i)
-      if x[0] == id:
-        accounts.update({i:x})
-    print( accounts)
-    time.sleep(10)
-            
-        
+  def showBalance(self,id):
+    accountData = self.dictAccounts.get(id)
+    print(f"\nLa cuenta {accountData[0]} tiene un saldo de {accountData[1]}")
+    
+  def found(self,id,qFound):
+    accountData = self.dictAccounts.get(id)
+    qFound = qFound + int(accountData[1])
+    accountData[1] = qFound
+    self.dictAccounts[id] = accountData
+    os.remove("accounts.txt")
+    
+    with open("accounts.txt","a") as file:
+      for i in self.dictAccounts:
+        x = self.dictAccounts.get(i)
+        file.write(f"\n{i},{x[0]},{x[1]}")
+    print(f"DEPOSITO REALIZADO CON ÉXITO, NUEVO SALDO : {qFound}")
+    time.sleep(4)
+    
+    
+  def withdrawal(self,id,qWithdrawal):
+    try:
+      accountData = self.dictAccounts.get(id)
+      balance = int(accountData[1])
+      
+      if qWithdrawal <= balance:
+        accountData[1] = balance - qWithdrawal
+        self.dictAccounts[id] = accountData
+        os.remove("accounts.txt")
+        balance = int(accountData[1])
+        with open("accounts.txt","a") as file:
+          for i in self.dictAccounts:
+            x = self.dictAccounts.get(i)
+            file.write(f"\n{i},{x[0]},{x[1]}")
+        print(f"RETIRO REALIZADO CON ÉXITO, NUEVO SALDO : {balance}")
+        time.sleep(4)
+        return True
+      else:
+        print("MONTO NO DISPONIBLE")
+        time.sleep(2) 
+    except ValueError :
+      print("Solo números")
+      time.sleep(3)
+  
+  
--- loop.py
+++ loop.py
@@ -1,3 +1,4 @@
+import os
 from time import sleep
 import time
 from accounts import Accounts
@@ -13,6 +14,7 @@ def Asking():
   if looking != None:
     print(f"\nHola {looking[0]}")
     Existing(id = id)
+    
   else:
     name = input("Nombre completo(solo letras): ")
     password = input("Contraseña(Solo números): ")
@@ -30,22 +32,45 @@ def Asking():
 def Existing( id = ""):
   l = "\n_____________________________\n"
   c = Clients()
+  a = Accounts()
   while True:
     data = c.lookingForUser(id)
     if id != "" and  data != None:
       password = input("\nIngresa tu contraseña( * para salir): ")
       if password == data[1]:
-        print(f"\n\nHola {data[0]}")
         break
       elif password== "*":
         exit()
     else:
       id = input("\nIngresa tu número de identificación: ").strip()
-      
-  selection = input(f"{l} \n1. Ver saldo\n2. Depositar\n3. Retirar\n4. Salir{l}\n")
-  if selection == "1":
-    a = Accounts()
-    a.showAccountsByID(id)
-  elif selection == "4":
-    exit()
+   
+  while True:   
+    selection = input(f"{l}\nHola {data[0]}\n\n1. Ver saldo\n2. Depositar\n3. Retirar\n4. Enviar dinero{l}\n")
+    if selection == "1":
+      a.showBalance(id)
+      time.sleep(3)
+    elif selection == "2":
+      qFound = int(input("\n¿Cuanto dinero quieres depositar?"))
+      a.found(id,qFound)
+    elif selection == "3":
+      qWithdrawal = int(input(f"\n¿Cuanto dinero quieres retirar?  "))
+      a.withdrawal(id,qWithdrawal)
+    
+    elif selection == "4":
+      receiverID = input("\nIngresa tu número de identificación del destinatario: ").strip()
+      inClients = c.lookingForUser(receiverID)
+      if inClients == None:
+        print("\nNO SE ENCONTRARON USER CON ESE ID")
+      else:
+        qWithdrawal = int(input(f"\n¿Cuanto dinero quieres enviar?  "))
+        withdrawal = a.withdrawal(id,qWithdrawal)
+        if withdrawal == True:
+          qFound = qWithdrawal
+          a.found(receiverID,qFound)
+          os.system("cls")
+          print("ENVIO DE DINERO EXITOSO")
+          break
+
+    else:
+      exit()
     
